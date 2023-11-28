a49b7130db468bf78cc6c2f3cedf91b431f62de6
pc2023 <hadeere378@gmail.com>
2023-11-28 15:07:49 +0200

    app colors & images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets{
+  static const String basePath='assets/images/';
+  static const String appLogo='${basePath}logo.svg';
+  static const String lock='${basePath}lock 1.png';
+  static const String imagePacker='${basePath}image_packer.png';
+  static const String background='${basePath}background.png';
+  static const String backgroundTwo='${basePath}background_image 2.png';
+}
